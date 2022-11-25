5ac0202df520f76ae16a6237698108d10fd1e2ac
interads <syarifuddin@interads.co.id>
2022-11-25 17:11:02 +0700

    Start Profile

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const router = useRouter();
+  useEffect(() => {
+    typeof document !== undefined
+      ? require("bootstrap/dist/js/bootstrap")
+      : null;
+  }, [router.events]);
+
   return <Component {...pageProps} />
 }
---
+++ pages/blog/index.tsx
@@ -0,0 +1,7 @@
+const Blog = () => {
+
+
+    return <p>Blog</p>
+}
+
+export default Blog
